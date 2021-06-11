b035467fd0564afc82f6a6e31f76fa8c39a792b3
Craig Gower-Page <craiggower@gmail.com>
2021-06-11 14:56:22 +0100

    added example testing file

---
+++ tests/testthat/test-example.R
@@ -0,0 +1,18 @@
+
+
+
+test_that( "some basic example",{
+    expect_equal(1,1)
+    expect_equal(1,1)
+    expect_equal(1,1)
+    expect_equal(1,2)
+    expect_equal(1,1)
+})
+
+test_that( "some basic example 2",{
+    expect_equal(1,1)
+    expect_equal(1,1)
+    expect_equal(1,1)
+    expect_equal(1,2)
+    expect_equal(1,1)
+})
