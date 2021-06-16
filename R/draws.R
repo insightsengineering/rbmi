910367771c23b9ecaf440c0a7d290c6610a1cd4c
Craig Gower-Page <craiggower@gmail.com>
2021-06-16 15:10:19 +0100

    initial implmentation of draws and scaling

--- R/draws.R
+++ R/draws.R
@@ -18,32 +18,23 @@ draws <- function(data, data_ice, vars, method){
     UseMethod("draws", method)
 }
 
-as_model_formula <- function(vars){
-    return(list())
-}
 
-as_simple_formula <- function(vars){
-    variables <- c(
-        vars$outcome,
-        vars$group,
-        vars$visit,
-        vars$covariates
-    )
-    frm <- as.formula(paste0( "~ -1 +", paste0( variables, collapse = " + ")))
-    return(frm)
-}
-
-
-as_design <- function(data, frm){
-    frame <- model.frame(frm, data = dat, na.action = NULL)
-    design_mat <- model.matrix(frm, frame)
-    design <- as.data.frame(design_mat)
-    return(design)
+#' @export
+draws.method_bootstrap <- function(data, data_ice, vars, method){
+    x <- draws_bootstrap(data, data_ice, vars, method)
+    as_class(x, "bootstrap")
 }
 
 
 #' @export
-draws.method_bootstrap <- function(data, data_ice, vars, method){
+draws.method_condmean <- function(data, data_ice, vars, method){
+    x <- draws_bootstrap(data, data_ice, vars, method)
+    as_class(x, "condmean")
+}
+
+
+#' @export
+draws_bootstrap <- function(data, data_ice, vars, method){
 
     stopifnot(
         is_validate_long(data, vars),
@@ -51,84 +42,92 @@ draws.method_bootstrap <- function(data, data_ice, vars, method){
     )
     longdata <- rmDataConstructor$new(data, vars)
 
-    design <- as_design(data, as_simple_formula(vars))
-    scaler <- scalerConstructor$new(design)
-    initial <- get_mmrm_coefs(
-        data = data,
-        vars = vars,
-        scaler = scaler,
-        unscale = FALSE
+    model_df <- as_model_df(data, as_simple_formula(vars))
+    scaler <- scalerConstructor$new(model_df)
+
+    model_df_scaled <- scaler$scale(model_df)
+
+    mmrm_initial <- fit_mmrm(
+        data = model_df_scaled[,-1],
+        outcome = model_df_scaled[,1],
+        ids = data[[vars$subjid]],
+        visits = data[[vars$visits]],
+        groups = data[[vars$groups]],
+        method = method
+    )
+
+    iniital_sample <- list(
+        beta = scaler$unscale_beta(mmrm_initial$beta),
+        sigma = scaler$unscale_sigma(mmrm_initial$sigma),
+        structure = mmrm_initial$structure,
+        ids = longdata$ids
     )
 
     samples <- replicate(
-        n = method$M,
+        n = method$M - 1,
         {
             get_bootstrap_mmrm_coefs(
-                longdata,
-                method,
+                longdata = longdata,
+                method = method,
                 scaler = scaler,
-                initial = initial
+                initial = mmrm_initial
             )
         },
         simplify = FALSE
     )
 
+    samples <- append(initial_sample, samples)
+
+    structures <- lappy(
+        samples,
+        function(x) x[["structure"]]
+    )
+
+    ## TODO - Code to summarise structures
+
     result <- list(
         method = method,
         data = longdata,
-        samples = samples
+        samples = samples,
+        structures = structures
     )
+
     return(result)
 }
 
 
-get_bootstrap_mmrm_coefs <- function(longdata, method, ...){
+get_bootstrap_mmrm_coefs <- function(longdata, scaler = NULL, ...){
+    vars <- longdata$vars
     ids_boot <- longdata$sample_ids()
     dat_boot <- longdata$get_data(ids_boot)
 
-    coefs <- get_mmrm_coefs(dat_boot, longdata$vars, ...)
+    model_df <- as_model_df(dat_boot, as_simple_formula(vars))
+
+    if(!is.null(scaler)){
+        model_df <- scaler$scale(model_df)
+    }
+
+    mmrm <- fit_mmrm(
+        data = model_df[,-1],
+        outcome = model_df[,1],
+        ids = dat_boot[[vars$subjid]],
+        visits = dat_boot[[vars$visits]],
+        groups = dat_boot[[vars$groups]],
+        ...
+    )
 
     result <- list(
-        beta = coefs$beta,
-        sigma = coef$sigma,
+        beta = scaler$unscale_beta(mmrm$beta),
+        sigma = scaler$unscale_sigma(mmrm$sigma),
+        structure = mmrm$structure,
         ids = ids_boot
     )
     return(result)
 }
 
 
-get_mmrm_coefs <- function(data, vars, scaler = NULL, initial = NULL, unscale = TRUE){
-    design <- as_design(data, as_simple_formula(vars))
-    design_scaled <- scaler$scale(design)
-
-    mmrm <- fit_mmrm(
-        data = design_scaled,
-        ids = data[[vars$subjid]],
-        visits = data[[vars$visits]],
-        method = method,
-        initialise = initialise
-    )
-
-    coefs <- extract_mmrm(mmrm)
-
-    if(unscale){
-        sigma <- scaler$unscale_sigma(coefs$sigma)
-        beta <- scaler$unscale_sigma(coefs$beta)
-    } else {
-        sigma <- coefs$sigma
-        beta <- coefs$beta
-    }
 
 
-    results <- list(
-        beta = beta,
-        sigma = sigma
-    )
-
-    return(results)
-}
-
-draws(iris, iris2, list(), method_bootstrap())
 
 
 
--- R/scaling.R
+++ R/scaling.R
@@ -1,2 +1,190 @@
 
 
+scalerConstructor <- R6::R6Class(
+    classname = "scaler",
+    public = list(
+
+        center = NULL,
+        scales = NULL,
+        cat_flag = NULL,
+
+
+        initialize = function(dat){
+
+            stopifnot(
+                is.data.frame(dat) | is.matrix(dat),
+                all( vapply(dat, is.numeric, logical(1)))
+            )
+
+            cat_flag <- vapply(
+                X = dat,
+                FUN = function(x) all(x %in% c(0,1)),
+                FUN.VALUE = logical(1),
+                USE.NAMES = FALSE
+            )
+
+            center <- vapply(
+                X = dat,
+                FUN = function(x) mean(x, na.rm = TRUE),
+                FUN.VALUE = numeric(1),
+                USE.NAMES = FALSE
+            )
+
+            scales <- vapply(
+                X = dat,
+                FUN = function(x) sd(x, na.rm = TRUE),
+                FUN.VALUE = numeric(1),
+                USE.NAMES = FALSE
+            )
+
+            center[cat_flag] <- 0
+            scales[cat_flag] <- 1
+            self$center <- center
+            self$scales <- scales
+            self$cat_flag <- cat_flag
+        },
+
+        scale = function(dat){
+            stopifnot(
+                ncol(dat) == length(self$center),
+                is.data.frame(dat) | is.matrix(dat),
+                all( vapply(dat, is.numeric, logical(1)))
+            )
+
+            dat <- sweep(
+                dat,
+                MARGIN = 2,
+                STATS = self$center,
+                FUN = `-`
+            )
+
+            dat <- sweep(
+                dat,
+                MARGIN = 2,
+                STATS = self$scales,
+                FUN = `/`
+            )
+
+            return(dat)
+        },
+
+        unscale_sigma = function(sigma){
+            stopifnot(is.matrix(sigma))
+            return( sigma / self$scales[[1]]^2)
+        },
+
+        unscale_beta = function(beta){
+            stopifnot(
+                is.numeric(beta),
+                length(beta) == (length(self$center) - 1)
+            )
+
+            b_0 <- beta[1]
+            b_i <- beta[-1]
+
+            mu_y <- self$center[1]
+            mu_i <- self$center[-c(1,2)]
+
+            sig_y <- self$scales[1]
+            sig_i <- self$scales[-c(1,2)]
+
+            unscaled_beta <- c(
+                mu_y + sig_y * b_0 - sum(sig_y * b_i * mu_i / sig_i),
+                b_i * sig_y / sig_i
+            )
+            return(unscaled_beta)
+        }
+    )
+)
+
+
+
+as_simple_formula <- function(vars){
+    variables <- c(
+        vars$group,
+        vars$visit,
+        vars$covariates
+    )
+    frm <- as.formula(
+        paste0(
+            vars$outcome,
+            "~ 1 + ",
+            paste0( variables, collapse = " + " )
+        )
+    )
+    return(frm)
+}
+
+
+as_model_df <- function(dat, frm){
+    design_mat <- model.matrix(frm, dat)
+    stopifnot( nrow(design_mat) == nrow(dat) )
+    outcome <- as.character(attr(terms(frm), "variables")[[2]])
+    full_mat <- cbind(dat[[outcome]] , design_mat)
+    design <- as.data.frame(full_mat)
+    return(design)
+}
+
+
+
+
+
+
+
+
+
+
+
+# ####### Test 1
+#
+#
+# vars <- list(
+#     outcome = "Sepal.Length",
+#     group = "Sepal.Width",
+#     visit = "Petal.Length",
+#     covariates = c("Species", "Species*Petal.Length")
+# )
+#
+# model_df <- as_model_df(iris, as_simple_formula(vars))
+# scaler <- scalerConstructor$new(model_df)
+#
+# model_df_scaled <- scaler$scale(model_df)
+#
+# outcome_scaled <- model_df_scaled[,1]
+# design_scaled <- model_df_scaled[,-1]
+# mod_scaled <- lm(outcome_scaled ~ . - 1, data = design_scaled )
+#
+# outcome <- model_df[,1]
+# design <- model_df[,-1]
+# mod <- lm(outcome ~ . - 1, data = design )
+#
+# mod$coefficients - scaler$unscale_beta(mod_scaled$coefficients)
+#
+#
+# ####### Test 2
+#
+#
+# vars <- list(
+#     outcome = "Sepal.Length",
+#     group = "Sepal.Width",
+#     visit = "Petal.Length",
+#     covariates = c("Species", "Species*Petal.Length", "newvar * Species", "newvar * Sepal.Width")
+# )
+#
+# i2 <- iris
+# i2$newvar <- factor(sample(c("A", "B", "C"), size = 150 , replace = TRUE))
+#
+# model_df <- as_model_df(i2, as_simple_formula(vars))
+# scaler <- scalerConstructor$new(model_df)
+#
+# model_df_scaled <- scaler$scale(model_df)
+#
+# outcome_scaled <- model_df_scaled[,1]
+# design_scaled <- model_df_scaled[,-1]
+# mod_scaled <- lm(outcome_scaled ~ . - 1, data = design_scaled )
+#
+# outcome <- model_df[,1]
+# design <- model_df[,-1]
+# mod <- lm(outcome ~ . - 1, data = design )
+#
+# mod$coefficients - scaler$unscale_beta(mod_scaled$coefficients)
