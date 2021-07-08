074958f4231888ce461dcca3c8f4da4d7dd40bc3
Alessandro Noci <alessandro.noci@roche.com>
2021-07-08 17:30:05 +0200

    4 tabs indentation

--- R/mcmc.R
+++ R/mcmc.R
@@ -12,21 +12,21 @@ long2wide <- function(vec_long,
 }
 
 get_obs_missingness_patterns <- function(outcome) {
-  
+    
     assert_that(
         is.matrix(outcome),
         msg = "outcome must be a matrix"
     )
-  
+    
     y_obs <- matrix(1, nrow = nrow(outcome), ncol = ncol(outcome))
     y_obs[is.na(outcome)] <- 0
     missingness_patterns <- unique(y_obs)
     M <- apply(y_obs,
                1,
                function(y) which(apply(missingness_patterns, 1, function(x) identical(x, y))==TRUE))
-  
-  return(list(missingness_patterns = missingness_patterns,
-              M = M))
+    
+    return(list(missingness_patterns = missingness_patterns,
+                M = M))
 }
 
 prepare_data_mcmc <- function(outcome,
@@ -35,26 +35,26 @@ prepare_data_mcmc <- function(outcome,
                               R,
                               same_cov,
                               Sigma_reml) {
-  
+    
     assert_that(
         is.list(Sigma_reml),
         msg = "Sigma_reml must be a list of covariance matrices"
     )
-  
+    
     assert_that(
         is.factor(groups),
         msg = "groups must be a factor"
     )
-  
+    
     J <- nrow(Sigma_reml[[1]])
-  
+    
     outcome <- long2wide(vec_long = outcome,
                          J = J)
     N <- nrow(outcome)
-  
+    
     obs_miss_patterns <- get_obs_missingness_patterns(outcome)
     outcome[is.na(outcome)] <- 1000
-  
+    
     if(same_cov) {
         data <- list(J = J,
                      N = N,
@@ -67,23 +67,23 @@ prepare_data_mcmc <- function(outcome,
                      y_observed = obs_miss_patterns$missingness_patterns,
                      Sigma_reml = Sigma_reml[[1]])
     } else {
-      G <- nlevels(groups)
-      which_arm <- as.numeric(groups)[seq(1, N*J, by = J)]
-      
-      data <- list(J = J,
-                   N = N,
-                   P = ncol(Q),
-                   n_missingness_patterns = nrow(obs_miss_patterns$missingness_patterns),
-                   M = obs_miss_patterns$M,
-                   Q = Q,
-                   R = R,
-                   y = outcome,
-                   y_observed = obs_miss_patterns$missingness_patterns,
-                   Sigma_reml = Sigma_reml,
-                   G = G,
-                   which_arm = which_arm)
+        G <- nlevels(groups)
+        which_arm <- as.numeric(groups)[seq(1, N*J, by = J)]
+        
+        data <- list(J = J,
+                     N = N,
+                     P = ncol(Q),
+                     n_missingness_patterns = nrow(obs_miss_patterns$missingness_patterns),
+                     M = obs_miss_patterns$M,
+                     Q = Q,
+                     R = R,
+                     y = outcome,
+                     y_observed = obs_miss_patterns$missingness_patterns,
+                     Sigma_reml = Sigma_reml,
+                     G = G,
+                     which_arm = which_arm)
     }
-  
+    
 }
 
 #' @importFrom rstan sampling
@@ -94,7 +94,7 @@ run_mcmc <- function(
     burn_in,
     burn_between,
     initial_values) {
-  
+    
     suppressWarnings({
         stan_fit <- sampling(
             object = stan_model,
@@ -106,62 +106,62 @@ run_mcmc <- function(
             iter = burn_in + burn_between*n_imputations,
             init = initial_values)
     })
-  
+    
     return(stan_fit)
-  
+    
 }
 
 #' @importFrom rstan extract
 extract_draws <- function(stan_fit) {
-  
+    
     return(extract(stan_fit, pars = c("beta", "Sigma")))
 }
 
 #' @importFrom rstan summary
 get_ESS <- function(stan_fit) {
-
+    
     return(rstan::summary(stan_fit, pars = c("beta", "Sigma"))$summary[,"n_eff"])
 }
 
 check_ESS <- function(stan_fit, n_draws, threshold = 0.4) {
-  
+    
     ESS <- get_ESS(stan_fit)
-  
+    
     n_low_ESS <- sum(ESS/n_draws < threshold)
-  
+    
     if(any(ESS/n_draws < threshold)) {
         warning(paste0("The Effective Sample Size is below ",threshold*100,"% for ", n_low_ESS ," parameters. Please consider increasing burn-in and/or burn-between"),
                 call. = FALSE)
     }
-  
+    
     return(invisible(NULL))
 }
 
 #' @importFrom rstan get_divergent_iterations get_bfmi get_max_treedepth_iterations
 check_hmc_diagn <- function(stan_fit) {
-  
+    
     if(any(get_divergent_iterations(stan_fit)) # draws "out of the distribution"
-        | isTRUE(get_bfmi(stan_fit) < 0.2 ) # exploring well the target distribution
-        | any(get_max_treedepth_iterations(stan_fit))) # efficiency of the algorithm
+       | isTRUE(get_bfmi(stan_fit) < 0.2 ) # exploring well the target distribution
+       | any(get_max_treedepth_iterations(stan_fit))) # efficiency of the algorithm
     {
         warning("Lack of efficiency in the HMC sampler: please consider increasing the burn-in period.",
                 call. = FALSE)
     } 
-  
+    
     return(invisible(NULL))
 }
 
 check_mcmc <- function(stan_fit, threshold_lowESS = 0.4) {
-  
+    
     pars <- extract_draws(stan_fit)
     n_draws <- nrow(pars$beta)
-  
+    
     check_ESS(
         stan_fit = stan_fit,
         n_draws = n_draws,
         threshold = threshold_lowESS
-        )
-  
+    )
+    
     check_hmc_diagn(stan_fit)
     
     return(invisible(NULL))
--- tests/testthat/test-mcmc.R
+++ tests/testthat/test-mcmc.R
@@ -1,29 +1,29 @@
 test_that("transformation from long to wide format works", {
     vec_long = c(c(NA, 1, 2), c(1, NA, 2), c(3, NA, 4))
     vec_wide_true = rbind(c(NA, 1, 2), c(1, NA, 2), c(3, NA, 4))
-  
+    
     vec_wide = long2wide(vec_long = vec_long,
                          J = 3)
-  
+    
     vec_wide_oneElem = long2wide(vec_long = vec_long[1:3],
                                  J = 3)
-  
+    
     expect_true(is.matrix(vec_wide))
     expect_equal(vec_wide, vec_wide_true)
-  
+    
     expect_true(is.matrix(vec_wide_oneElem))
     expect_equal(vec_wide_oneElem, vec_wide_true[1,, drop = FALSE])
 })
 
 test_that("Proper missingness patterns are detected", {
     outcome = rbind(c(NA, 1, 2), c(1, NA, 2), c(3, NA, 4))
-  
+    
     res = get_obs_missingness_patterns(outcome)
     missingness_patterns = res$missingness_patterns
     M = res$M
-  
+    
     number_missingness_patterns = nrow(missingness_patterns)
-  
+    
     expect_equal(number_missingness_patterns, 2)
     expect_equal(missingness_patterns, rbind(c(0,1,1),c(1,0,1)))
     expect_equal(M, c(1,2,2))
@@ -31,13 +31,13 @@ test_that("Proper missingness patterns are detected", {
 
 test_that("Proper missingness pattern is detected when only one patient", {
     outcome = t(matrix(c(NA, 1, 2)))
-  
+    
     res = get_obs_missingness_patterns(outcome)
     missingness_patterns = res$missingness_patterns
     M = res$M
-  
+    
     number_missingness_patterns = nrow(missingness_patterns)
-  
+    
     expect_equal(number_missingness_patterns, 1)
     expect_equal(missingness_patterns, t(matrix(c(0,1,1))))
     expect_equal(M, 1)
