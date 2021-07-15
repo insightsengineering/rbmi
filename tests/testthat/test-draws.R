193d5e661bbc8a4df27d1f21e4d41bf3301bfe9b
Alessandro Noci <alessandro.noci@roche.com>
2021-07-15 15:21:10 +0200

    update with roxigen

---
+++ tests/testthat/test-draws.R
@@ -0,0 +1,47 @@
+library(dplyr)
+library(testthat)
+
+n <- 4
+nv <- 3
+
+covars <- tibble(
+    subjid = 1:n,
+    age = rnorm(n),
+    group = factor(sample(c("A", "B"), size = n, replace = TRUE), levels = c("A", "B")),
+    sex = factor(sample(c("M", "F"), size = n, replace = TRUE), levels = c("M", "F")),
+    strata = c("A","A", "A", "B")
+)
+
+dat <- tibble(
+    subjid = rep.int(1:n, nv)
+) %>%
+    left_join(covars, by = "subjid") %>%
+    mutate( outcome = rnorm(
+        n(),
+        age * 3 + (as.numeric(sex) - 1) * 3 + (as.numeric(group) - 1) * 4,
+        sd = 3
+    )) %>%
+    arrange(subjid) %>%
+    group_by(subjid) %>%
+    mutate( visit = factor(paste0("Visit ", 1:n())))  %>%
+    ungroup() %>%
+    mutate(subjid = as.character(subjid))
+
+dat[c(1,2,3,4,6,7), "outcome"] <- NA
+
+
+vars <- list(
+    outcome = "outcome",
+    visit = "visit",
+    subjid = "subjid",
+    group = "group",
+    strata = "strata",
+    covariates = c("sex", "age"),
+    method = "method"
+)
+
+ld <- longDataConstructor$new(
+    data = dat,
+    vars = vars
+)
+
