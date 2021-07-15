356419ccd975dd5ffe4cd381c6cf27fc50d84811
Alessandro Noci <alessandro.noci@roche.com>
2021-07-15 23:34:28 +0200

    remove some commented code

--- R/draws.R
+++ R/draws.R
@@ -1,18 +1,3 @@
-# draws(
-#     data = data.frame(...),               # Dataset with all required variables
-#     vars = list(
-#         visit = “visit”,                 # Factor J-levels
-#         group = “arm”,                   # Factor L-levels
-#         method = “method”                # Character fixed values i.e. MAR, JTR, etc
-#         subjid = “subjid”,               # Character or factor ?
-#         outcome = “outcome”,           # Numeric continuous
-#         covariates = c(“c1”, “c2”, “c1*c3”, “base_out*visit”)
-#     ),
-#     data_ice = data.frame(),
-#     method = method_obj()                  #  A "method" object
-# )
-
-
 #' TODO
 #'
 #' @name draws
