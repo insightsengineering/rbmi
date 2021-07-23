97b993865352506ca0830a0c860aa18c75fd13c2
Craig Gower-Page <craig.gower-page@roche.com>
2021-07-23 10:24:24 +0100

    futher pool udate

--- R/pool.R
+++ R/pool.R
@@ -1,75 +1,5 @@
 
 
-
-
-
-
-
-
-#' Transpose Results Object
-#' 
-#' Transposes a Results object (as created by analyse()) inorder to group 
-#' the same estimates together into vectors. 
-#' 
-#' @param results TODO
-transpose_results <- function(results){
-    elements <- names(results[[1]])
-    results_transpose <- list()
-
-    for( element in elements){
-        results_transpose[[element]] = list(
-            est = vapply(results, function(x) x[[element]][["est"]], numeric(1)),
-            se = vapply(results, function(x) x[[element]][["se"]], numeric(1)),
-            df = vapply(results, function(x) x[[element]][["df"]], numeric(1))
-        )
-    }
-    return(results_transpose)
-}
-
-
-
-#' Title
-#' 
-#' @description 
-#' TODO
-#' @param point TODO
-#' @param se TODO
-#' @param alpha TODO
-#' @param alternative TODO
-#' @param qfun TODO
-#' @param pfun TODO
-#' @param ... TODO
-normal_ci <- function( point, se, alpha, alternative, qfun, pfun, ...){
-    ci <- switch(
-        alternative,
-        two.sided = c(-1, 1) * qfun(1 - alpha / 2, ...),
-        greater =  c(-Inf, 1) *  qfun(1 - alpha, ...),
-        less = c(-1, Inf) * qfun(1- alpha, ...)
-    ) * se + point
-    
-    pvals <- c(
-        pfun(point, sd = se, lower.tail = TRUE, ...),
-        pfun(point, sd = se, lower.tail = FALSE, ...)
-    )
-    
-    index <- switch(
-        alternative,
-        two.sided = c(1, 2),
-        greater = 1,
-        less = 2
-    )
-    
-    ret <- list(
-        est = point,
-        ci = ci,
-        pvalue = min(pvals[index]) * length(pvals[index])
-    )
-    return(ret)
-}
-
-
-
-
 #' TODO
 #'
 #' @name pool
@@ -78,19 +8,34 @@ normal_ci <- function( point, se, alpha, alternative, qfun, pfun, ...){
 #'
 #' @export
 pool <- function(
-    results, 
+    results,
     conf.level = 0.95,
     alternative = c("two.sided", "less", "greater"),
     ...
-){
+) {
+
     alternative <- match.arg(alternative)
+
+    assert_that(
+        is.numeric(conf.level),
+        length(conf.level) == 1,
+        0 < conf.level & conf.level < 1,
+        msg = "`conf.level` must be between 0 and 1"
+    )
+
     validate_analysis_results(results)
     results_transpose <- transpose_results(results)
-    ret <- lapply(
-        results_transpose, 
-        pool_,
+    pars <- lapply(
+        results_transpose,
+        getS3method("pool_", class(results)),
         conf.level,
-        alternative
+        alternative,
+        ...
+    )
+    ret <- list(
+        pars = pars,
+        conf.level = conf.level,
+        alternative = alternative
     )
     return(ret)
 }
@@ -98,9 +43,10 @@ pool <- function(
 
 #' @rdname pool
 #' @export
-pool_ <- function(results, conf.level, alternative, ...){
+pool_ <- function(results, conf.level, alternative, ...) {
     UseMethod("pool_")
 }
+
 
 
 #' @rdname pool
@@ -120,26 +66,73 @@ pool_.jackknife <- function(results, conf.level, alternative, ...){
 
 
 
-
 #' @rdname pool
 #' @export
 pool_.bootstrap <- function(
-    results, 
-    conf.level, 
-    alternative, 
-    type = c("percentile", "normal"), 
+    results,
+    conf.level,
+    alternative,
+    type = c("percentile", "normal"),
     ...
-){
-    type = match.arg(type)
+) {
+    type <- match.arg(type)
     bootfun <- switch(
         type,
         percentile = pool_bootstrap_percentile,
         normal = pool_bootstrap_normal
     )
-    
+
     ret <- bootfun(results$est, conf.level, alternative)
     return(ret)
 }
+
+
+
+
+#' @rdname pool
+#' @export
+pool_.rubin <- function(results, conf.level, alternative, ...) {
+    ests <- results$est
+    ses <- results$se
+    dfs <- results$df
+    alpha <- 1 - conf.level
+
+    M <- length(ests)
+    est_point <- mean(ests)
+
+    var_w <- mean(ses^2)
+    var_b <- var(ests)
+    var_t <- var_w + (1 + 1 / M) * var_b
+
+    v_com <- unique(dfs)
+
+    assert_that(
+        length(v_com) == 1,
+        msg = "Degrees of freedom should be consistent across all samples"
+    )
+
+    if (is.na(v_com) | is.infinite(v_com)) {
+        df <- Inf
+    } else {
+        lambda <- (1 + 1 / M) * var_b / var_t
+        v_obs <- ((v_com + 1) / (v_com + 3)) * v_com * (1 - lambda)
+        v_old <- (M - 1)  / lambda^2
+        df <- (v_old * v_obs) / (v_old + v_obs)
+    }
+
+    ret <- normal_ci(
+        point = est_point,
+        se = sqrt(var_t),
+        alpha = alpha,
+        alternative = alternative,
+        qfun = function(p, ...) qt(p, df = df),
+        pfun = function(q, lower.tail, ...) pt(q, df = df, lower.tail = lower.tail)
+    )
+
+    return(ret)
+}
+
+
 
 
 #' Title
@@ -148,20 +141,28 @@ pool_.bootstrap <- function(
 #' @param est TODO
 #' @param conf.level TODO
 #' @param alternative TODO
-pool_bootstrap_percentile <- function(est, conf.level, alternative){
+pool_bootstrap_percentile <- function(est, conf.level, alternative) {
     alpha <- 1 - conf.level
-    pvals <- (c(sum(est < 0),sum(est > 0)) + 1) / (length(est) + 1)
-    
-    index <- switch(
-        alternative,
+    pvals <- (c(sum(est < 0), sum(est > 0)) + 1) / (length(est) + 1)
+
+    index <- switch(alternative,
         two.sided = c(1, 2),
         greater = 1,
         less = 2
     )
-    
+
+    quant_2_side <- quantile(est, probs = c(alpha / 2, 1 - alpha / 2), type = 6, names = FALSE)
+    quant_1_side <- quantile(est, probs = c(alpha, 1 - alpha), type = 6, names = FALSE)
+
+    ci <- switch(alternative,
+        two.sided = quant_2_side,
+        greater = c(-Inf, quant_1_side[2]),
+        less = c(quant_1_side[1], Inf)
+    )
+
     ret <- list(
         est = est[1],  # First estimate should be original dataset
-        ci = quantile(est, probs = c(alpha / 2, 1 - alpha / 2), type = 6, names = FALSE),
+        ci = ci,
         pvalue = min(pvals[index]) * length(pvals[index])
     )
     return(ret)
@@ -169,7 +170,7 @@ pool_bootstrap_percentile <- function(est, conf.level, alternative){
 
 
 #' Title
-#' @description 
+#' @description
 #' TODO
 #' @param est TODO
 #' @param conf.level TODO
@@ -184,37 +185,64 @@ pool_bootstrap_normal <- function(est, conf.level, alternative) {
 
 
 
-#' @rdname pool
-#' @export
-pool_.rubin <- function(results, ...) {
-    ests <- results$est
-    ses <- results$se
-    dfs <- results$df
-    
-    M <- length(ests)
-    est_point <- mean(ests)
-    
-    var_w <- mean(ses^2)
-    var_b <- var(ests)
-    var_t <- var_w + (1 + 1 / M) * var_b
-    
-    v_com <- unique(results$df)
-    
-    asser_that(
-        length(v_com) == 1,
-        msg = "Degrees of freedom should be consistent across all samples"
+#' Title
+#'
+#' @description
+#' TODO
+#' @param point TODO
+#' @param se TODO
+#' @param alpha TODO
+#' @param alternative TODO
+#' @param qfun TODO
+#' @param pfun TODO
+normal_ci <- function(point, se, alpha, alternative, qfun, pfun) {
+    ci <- switch(
+        alternative,
+        two.sided = c(-1, 1) * qfun(1 - alpha / 2),
+        greater =  c(-Inf, 1) *  qfun(1 - alpha),
+        less = c(-1, Inf) * qfun(1 - alpha)
+    ) * se + point
+
+    pvals <- c(
+        pfun(point, sd = se, lower.tail = TRUE),
+        pfun(point, sd = se, lower.tail = FALSE)
     )
-    
-    if( is.na(v_com) | is.infinite(v_com)){
-        df <- Inf
-    } else {
-        lambda <- (1 + 1 / M) * var_b / var_t
-        v_obs <- ((v_com + 1) / (v_com + 3)) * v_com * (1 - lambda)
-        v_old <- (M - 1)  / lambda^2
-        df <- (v_old * v_obs) / (v_old + v_obs)     
-    }
-    
-    ret <- normal_ci(est_point, sqrt(var_t), alpha, alternative, qt, pt, df = df)
-    
+
+    index <- switch(
+        alternative,
+        two.sided = c(1, 2),
+        greater = 2,
+        less = 1
+    )
+
+    ret <- list(
+        est = point,
+        ci = ci,
+        pvalue = min(pvals[index]) * length(pvals[index])
+    )
     return(ret)
+}
+
+
+
+
+
+#' Transpose Results Object
+#'
+#' Transposes a Results object (as created by analyse()) inorder to group
+#' the same estimates together into vectors.
+#'
+#' @param results TODO
+transpose_results <- function(results) {
+    elements <- names(results[[1]])
+    results_transpose <- list()
+
+    for (element in elements) {
+        results_transpose[[element]] <- list(
+            est = vapply(results, function(x) x[[element]][["est"]], numeric(1)),
+            se = vapply(results, function(x) x[[element]][["se"]], numeric(1)),
+            df = vapply(results, function(x) x[[element]][["df"]], numeric(1))
+        )
+    }
+    return(results_transpose)
 }
--- tests/testthat/test-pool.R
+++ tests/testthat/test-pool.R
@@ -1,42 +1,83 @@
 
 
 
-
-results <- list(
-    list(
-        "p1" = list(
-            "est" = 1.23,
-            "se" = 1.13,
-            "df" = 12
-        ),
-        "p2" = list(
-            "est" = 1.23,
-            "se" = 1.13,
-            "df" = 12
-        )
-    ),
-    list(
-        "p1" = list(
-            "est" = 1.23,
-            "se" = 1.13,
-            "df" = 12
-        ),
-        "p2" = list(
-            "est" = 1.23,
-            "se" = 1.13,
-            "df" = 12
-        )
-    )
-)
+# ########################
+# #
+# #  Bootstrap
+# #
+# #
 
 
-x <- rnorm(10000000, 1.96)
-x <- c(mean(x), x)
+# vals <- rnorm(500, 0, 1)
+# samp <- lapply(
+#     seq_len(length(vals)),
+#     function(x){
+#         vals2 <- sample(vals, size = length(vals), replace = TRUE)
+#         list("p1" = list("est" = mean(vals2), "se" = sd(vals2), "df" = NA))
+#     }
+# )
+# real <- list(list("p1" = list("est" = mean(vals), "se" = sd(vals), "df" = NA)))
+# results <- append(real, samp) 
 
-pool_bootstrap_normal(x, 0.95, "two.sided")
-pool_bootstrap_percentile(x, 0.95, "two.sided")
+# results <- results %>% as_class("bootstrap")
+
+# ### reference CIs 
+#  c(-1, 1) * 1.96 * sqrt(1 / 500) + mean(vals)
+
+# pool(results, type = "normal")
+# pool(results, type = "percentile")
+
+# pool(results, type = "normal", alternative = "less")
+# pool(results, type = "percentile", alternative = "less")
+
+# pool(results, type = "normal", alternative = "greater")
+# pool(results, type = "percentile", alternative = "greater")
+
+
+# results <- results %>% as_class("rubin")
+# pool(results)
+
+
+# ########################
+# #
+# #  Jackknife
+# #
+# #
+
+
+# vals <- rnorm(500, 0, 1)
+# samp <- lapply(
+#     seq_len(length(vals)),
+#     function(x){
+#         vals2 <- vals[-x]
+#         list("p1" = list("est" = mean(vals2), "se" = sd(vals2), "df" = NA))
+#     }
+# )
+# real <- list(list("p1" = list("est" = mean(vals), "se" = sd(vals), "df" = NA)))
+# results <- append(real, samp) %>% as_class("jackknife")
+
+# ### reference CIs 
+#  c(-1, 1) * 1.96 * sqrt(1 / 500) + mean(vals)
+
+# pool(results)
+# pool(results, alternative = "less")
+# pool(results, alternative = "greater")
 
 
 
 
+
+
+
+
+
+
+
+
+
+# x <- rnorm(1000000, 0)
+# x <- c(mean(x), x)
+
+# pool_bootstrap_normal(x, 0.95, "less")
+# pool_bootstrap_percentile(x, 0.95, "less")
 
