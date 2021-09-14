dd0b296d5172634b4b36ff428ba4b0df61dfa00a
Craig Gower-Page <craig.gower-page@roche.com>
2021-09-14 16:22:28 +0100

    added strategy documentation

--- R/strategies.R
+++ R/strategies.R
@@ -1,16 +1,27 @@
+
+
+
 #' Compute covariance matrix for some reference-based methods (JR, CIR)
 #'
 #' @description
 #' Adapt covariance matrix in reference-based methods. Used for Copy Increments in
 #' Reference (CIR) and Jump To Reference (JTR) methods, to adapt the covariance matrix
-#' to different pre-deviation and post deviation covariance structures.
+#' to different pre-deviation and post deviation covariance structures. See Carpenter
+#' et al. (2013)
 #'
-#' @param sigma_group TODO
-#' @param sigma_ref TODO
-#' @param index_mar TODO
+#' @param sigma_group the covariance matrix with dimensions equal to `index_mar` for
+#' the subjects original group
+#' @param sigma_ref the covariance matrix with dimensions equal to `index_mar` for
+#' the subjects reference group
+#' @param index_mar A logical vector indicating which visits meet the MAR assumption
+#' for the subject. I.e. this identifies the observations that after a non-MAR
+#' intercurrent event (ICE).
 #'
 #' @references
-#' TODO    insertRef{carpenter2013analysis}{rbmi}
+#' Carpenter, James R., James H. Roger, and Michael G. Kenward. "Analysis of longitudinal
+#' trials with protocol deviation: a framework for relevant, accessible assumptions, and
+#' inference via multiple imputation." Journal of biopharmaceutical statistics 23.6 (2013):
+#' 1352-1371.
 compute_sigma <- function(sigma_group, sigma_ref, index_mar){
 
     if(identical(sigma_group, sigma_ref)) {
@@ -55,14 +66,45 @@ compute_sigma <- function(sigma_group, sigma_ref, index_mar){
 }
 
 
-#' Title
+
+#' Strategies
 #'
-#' @description
-#' TODO
+#' These functions are used to implement various reference based imputation
+#' strategies by combining a subjects own distribution with that of
+#' a reference distribution based upon which of their visits failed to meet
+#' the MAR assumption
 #'
-#' @param pars_group TODO
-#' @param pars_ref TODO
-#' @param index_mar TODO
+#' @param pars_group A list of parameters for the subject's group. See details.
+#'
+#' @param pars_ref A list of parameters for the subject's reference group. See details.
+#'
+#' @param index_mar A logical vector indicating which visits meet the MAR assumption
+#' for the subject. I.e. this identifies the observations that after a non-MAR
+#' intercurrent event (ICE).
+#'
+#' @details
+#'
+#' `pars_group` and `pars_ref` both must be a list containing elements `mu` and `sigma`.
+#' `mu` must be a numeric vector and `sigma` must be a square matrix symmetric covariance
+#' matrix with dimensions equal to the length of `mu` and `index_mar`. e.g.
+#' ```
+#' list(
+#'     mu = c(1,2,3),
+#'     sigma = matrix(c(1,2,3,4,5,6,7,8,9), nrow = 3, ncol = 3)
+#' )
+#' ```
+#'
+#' Users can define their own strategy functions and include them via the `strategies`
+#' argument to [impute()] using [getStrategies()]. That being said the following
+#' strategies are available "out the box":
+#'
+#' - Missing at Random (MAR)
+#' - Jump to Reference (JR)
+#' - Copy Reference (CR)
+#' - Copy Increments from Reference (CIR)
+#' - Last Mean Carried Forward (LMCF)
+#'
+#' @name stratgies
 #' @export
 strategy_MAR <- function(pars_group, pars_ref, index_mar){
     return(pars_group)
@@ -70,14 +112,7 @@ strategy_MAR <- function(pars_group, pars_ref, index_mar){
 
 
 
-#' Title
-#'
-#' @description
-#' TODO
-#'
-#' @param pars_group TODO
-#' @param pars_ref TODO
-#' @param index_mar TODO
+#' @rdname stratgies
 #' @export
 strategy_JR <- function(pars_group, pars_ref, index_mar){
 
@@ -105,14 +140,7 @@ strategy_JR <- function(pars_group, pars_ref, index_mar){
 }
 
 
-#' Title
-#'
-#' @description
-#' TODO
-#'
-#' @param pars_group TODO
-#' @param pars_ref TODO
-#' @param index_mar TODO
+#' @rdname stratgies
 #' @export
 strategy_CR <- function(pars_group, pars_ref, index_mar){
     return(pars_ref)
@@ -120,14 +148,7 @@ strategy_CR <- function(pars_group, pars_ref, index_mar){
 
 
 
-#' Title
-#'
-#' @description
-#' TODO
-#'
-#' @param pars_group TODO
-#' @param pars_ref TODO
-#' @param index_mar TODO
+#' @rdname stratgies
 #' @export
 strategy_CIR <- function(pars_group, pars_ref, index_mar){
 
@@ -157,14 +178,7 @@ strategy_CIR <- function(pars_group, pars_ref, index_mar){
 }
 
 
-#' Title
-#'
-#' @description
-#' TODO
-#'
-#' @param pars_group TODO
-#' @param pars_ref TODO
-#' @param index_mar TODO
+#' @rdname stratgies
 #' @export
 strategy_LMCF <- function(pars_group, pars_ref, index_mar){
 
@@ -189,27 +203,30 @@ strategy_LMCF <- function(pars_group, pars_ref, index_mar){
 
 
 
-#' Define imputation strategies
+#' Get Imputation Strategies
 #'
 #' Returns a list defining the imputation strategies to be used to create the
-#' multivariate normal distribution parameters by merging those of the source group and reference
-#' group per patient.
+#' multivariate normal distribution parameters by merging those of the source
+#' group and reference group per patient.
 #'
-#' By default Jump to Reference (JR), Copy Reference (CR),
-#' Copy Increments from Reference (CIR), Last Mean Carried Forward (LMCF) and
-#' Missing at Random (MAR) are defined.
+#' By default Jump to Reference (JR), Copy Reference (CR), Copy Increments from
+#' Reference (CIR), Last Mean Carried Forward (LMCF) and Missing at Random (MAR)
+#' are defined.
 #'
 #' The user can define their own strategy functions (or overwrite the pre-defined ones)
 #' by specifying a named input to the function i.e. `NEW = function(...) ...`.
 #' Only exception is MAR which cannot be overwritten
 #'
-#' All user defined functions must take 3 inputs `pars_group`, `pars_ref` & `index_mar`.
-#' `pars_group` and `pars_ref` are both lists with elements `mu` and `sigma` representing
-#' the multivariate normal distribution parameters for the subjects current and reference group
-#' respectively.  `index_mar` will be a logical vector specifying which visits the subject met
-#' the MAR assumption at. The function must return a list with elements `mu` and `sigma`.
+#' All user defined functions must take 3 inputs `pars_group`, `pars_ref` &
+#'  `index_mar`. `pars_group` and `pars_ref` are both lists with elements `mu`
+#' and `sigma` representing the multivariate normal distribution parameters for
+#' the subjects current and reference group respectively.  `index_mar` will be
+#' a logical vector specifying which visits the subject met the MAR assumption
+#' at. The function must return a list with elements `mu` and `sigma`. See
+#' [strategy_JR()] for an example.
 #'
-#' @param ... User defined methods to be added to the return list. Input must be a function
+#' @param ... User defined methods to be added to the return list. Input must
+#' be a function
 #'
 #' @examples
 #' \dontrun{
@@ -219,7 +236,6 @@ strategy_LMCF <- function(pars_group, pars_ref, index_mar){
 #'     JR = function(pars_group, pars_ref, index_mar) <more code>
 #' )
 #' }
-#'
 #' @export
 getStrategies <- function(...){
     user_strats <- list(...)
