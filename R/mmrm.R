4ce5c0d33ed305b75c769b1d14fcfa558eb16753
Craig Gower-Page <craiggower@gmail.com>
2021-09-24 10:43:12 +0100

    Update R/mmrm.R
    Co-authored-by: Alessandro Noci <59877596+nociale@users.noreply.github.com>

--- R/mmrm.R
+++ R/mmrm.R
@@ -165,7 +165,7 @@ extract_params <- function(fit) {
 #'
 #'
 #' @param designmat a `data.frame` or `matrix` containing the covariates to use in the MMRM model.
-#' dummy variables must already be expanded out I.e. via [stats::model.matrix()]. Cannot contain
+#' Dummy variables must already be expanded out, i.e. via [stats::model.matrix()]. Cannot contain
 #' any missing values
 #' @param outcome a numeric vector. The outcome value to be reggressed on in the mmrm model.
 #' @param subjid a character / factor vector. The subject identify use to link separate visits that belong to
