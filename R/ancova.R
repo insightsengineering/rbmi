4f1ad756519898a4fb5dbfdde4d26fed40be1a6a
Alessandro Noci <59877596+nociale@users.noreply.github.com>
2022-02-28 18:36:43 +0100

    Update R/ancova.R
    Co-authored-by: Craig Gower-Page <craig.gower-page@roche.com>

--- R/ancova.R
+++ R/ancova.R
@@ -41,8 +41,8 @@
 #'    ...
 #')
 #'```
-#' Please note that "ref" refers to the first level of `vars$group` which not necessarily
-#' coincides with the control arm. Analogously "alt" refers to the second level of `vars$group`.
+#' Please note that "ref" refers to the first factor level of `vars$group` which does not necessarily
+#' coincide with the control arm. Analogously, "alt" refers to the second factor level of `vars$group`.
 #' "trt" refers to the model contrast translating the mean difference between the second level and first level.
 #'
 #' If you want to include interaction terms in your model this can be done
