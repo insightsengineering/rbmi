d291fb31cf536a9c413ea7ebd59af97d2c6cb9fe
gowercr1 <craig.gower-page@novartis.com>
2025-08-18 12:44:36 +0100

    update version number

--- R/analyse.R
+++ R/analyse.R
@@ -58,7 +58,7 @@
 #' a linear transformation of the outcomes.
 #' Thus care is required when applying alternative analysis functions in this setting.
 #'
-#' Note that as of verion 1.4.2 rbmi provides the `ancova_m_groups()` function; this is an extension to
+#' Note that as of verion 1.5.0 rbmi provides the `ancova_m_groups()` function; this is an extension to
 #' the `ancova()` function which supports the analysis of more than 2 treatment arms.
 #' The `ancova()` function is left as the default for backwards compatibility as `ancova_m_groups()`
 #' uses a different naming structure.
